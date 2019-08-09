b836a93a0f4a2056ce9ba8ded4e4e8a42b93e39a
Pochang Lee <pochangl@usc.edu>
2019-08-09 10:35:21 +0800

    make a new file for demonstration

---
+++ JumpGameII/test_v4.py
@@ -0,0 +1,38 @@
+from unittest import TestCase
+from .v4_optimal import Solution
+
+
+class TestSolution(TestCase):
+    def run_test(self, data, expect):
+        result = Solution().jump(data)
+        self.assertEqual(result, expect)
+
+    def test_case1(self):
+        data = []
+        expect = 0
+        self.run_test(data=data, expect=expect)
+
+    def test_case2(self):
+        data = [1]
+        expect = 0
+        self.run_test(data=data, expect=expect)
+
+    def test_case3(self):
+        data = [3, 2, 1]
+        expect = 1
+        self.run_test(data=data, expect=expect)
+
+    def test_case4(self):
+        data = [2, 3, 1, 1, 4]
+        expect = 2
+        self.run_test(data=data, expect=expect)
+
+    def test_case5(self):
+        data = [1, 2, 3]
+        expect = 2
+        self.run_test(data=data, expect=expect)
+
+    def test_case6(self):
+        data = [5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0]
+        expect = 3
+        self.run_test(data=data, expect=expect)
