c551d2eccb4ec9f7ed380e225f28b2f724e95c23
pochangl <pochanglee2008@gmail.com>
2019-08-10 15:57:41 +0800

    修正程式, 讓它可以跑基本test case

--- BinaryTreeCamera/main.py
+++ BinaryTreeCamera/main.py
@@ -1,18 +1,25 @@
 from dsa.tree.binary.traversal import recursion
 from collections import namedtuple
 
-Result = namedtuple('Result', ['is_monitored', 'count'])
+
+Result = namedtuple('Result', ['is_monitored', 'has_camera', 'count'])
+
 
 def decision(node, left, right):
     if node is None:
-        return Result(is_monitored=True, count=0)
+        return Result(is_monitored=True, has_camera=False, count=0)
 
     count = left.count + right.count
 
     if left.is_monitored and right.is_monitored:
-        return Result(is_monitored=False, count=count)
+        return Result(
+            is_monitored=left.has_camera or right.has_camera,
+            has_camera=False,
+            count=count
+        )
     else:
-        return Result(is_monitored=True, count=count + 1)
+        return Result(is_monitored=True, has_camera=True, count=count + 1)
+
 
 class Solution:
     def minCameraCover(self, root) -> int:
