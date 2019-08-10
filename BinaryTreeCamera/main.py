5f0f9b47d915e986f5d27471282c5ea8181ba68d
pochangl <pochanglee2008@gmail.com>
2019-08-10 15:49:01 +0800

    寫第一支程式

--- BinaryTreeCamera/main.py
+++ BinaryTreeCamera/main.py
@@ -1,10 +1,20 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+from dsa.tree.binary.traversal import recursion
+from collections import namedtuple
+
+Result = namedtuple('Result', ['is_monitored', 'count'])
+
+def decision(node, left, right):
+    if node is None:
+        return Result(is_monitored=True, count=0)
+
+    count = left.count + right.count
+
+    if left.is_monitored and right.is_monitored:
+        return Result(is_monitored=False, count=count)
+    else:
+        return Result(is_monitored=True, count=count + 1)
 
 class Solution:
     def minCameraCover(self, root) -> int:
-        return -1
+        count = recursion(decision)
+        return count(root).count
