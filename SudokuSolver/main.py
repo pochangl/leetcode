6e433d1eb6b68e30f8058edab4791f25448f5c6f
Pochang Lee <pochangl@usc.edu>
2019-08-17 08:15:25 +0800

    寫下基本想法

--- SudokuSolver/main.py
+++ SudokuSolver/main.py
@@ -45,8 +45,21 @@ class Solution:
             cells[x][y] = Cell(x, y, board[x][y])
 
         # initial observers
-        for x, y in range(9):
-            pass  # 還不知道要寫什麼
+        for x, y in axis:
+            cell = cells[x][y]
+            for index in range(9):
+                # subscribe to x axis cells
+                cell.subscribe_to(cells[x][index])
+                # subscribe to y axis cells
+                cell.subscribe_to(cells[index][y])
+
+            deltas = permutations(range(3), 2)
+            base_x = x % 3
+            base_y = y % 3
+
+            for dx, dy in deltas:
+                # subscribe to local block
+                cell.subscribe_to(cells[base_x + dx][base_y + dy])
 
         # initial candidates
         for row in cells:
