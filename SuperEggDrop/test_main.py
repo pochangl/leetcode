ef373f1e6395e39dbedaebd40fd1ac9260cb179f
Pochang Lee <pochangl@usc.edu>
2019-08-18 16:53:45 +0800

    寫下能想到的簡單 test case

--- SuperEggDrop/test_main.py
+++ SuperEggDrop/test_main.py
@@ -15,3 +15,21 @@ class TestSolution(TestCase):
 
     def test_problem_case3(self):
         self.run_test(eggs=3, floors=14, expect=4)
+
+    def test_1_egg(self):
+        for floors in range(100):
+            self.run_test(eggs=1, floors=floors, expect=floors)
+
+    def test_1_floor(self):
+        for eggs in range(1, 10):
+            self.run_test(eggs=eggs, floors=1, expect=1)
+
+    def test_2_floors(self):
+        for eggs in range(1, 10):
+            self.run_test(eggs=eggs, floors=2, expect=2)
+
+    def test_3_floors(self):
+        self.run_test(eggs=2, floors=4, expect=3)
+
+        for eggs in range(3, 10):
+            self.run_test(eggs=eggs, floors=4, expect=3)
