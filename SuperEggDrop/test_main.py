67caab5d2ae4355a2f4c673bd465183c5e1b6e52
Pochang Lee <pochangl@usc.edu>
2019-08-18 16:36:34 +0800

    換名字

--- SuperEggDrop/test_main.py
+++ SuperEggDrop/test_main.py
@@ -7,11 +7,11 @@ class TestSolution(TestCase):
         result = Solution().superEggDrop(K=eggs, N=floors)
         self.assertEqual(result, expect)
 
-    def test_case1(self):
+    def test_problem_case1(self):
         self.run_test(eggs=1, floors=2, expect=2)
 
-    def test_case2(self):
+    def test_problem_case2(self):
         self.run_test(eggs=2, floors=6, expect=3)
 
-    def test_case3(self):
+    def test_problem_case3(self):
         self.run_test(eggs=3, floors=14, expect=4)
