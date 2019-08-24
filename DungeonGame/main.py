683d0d27874e4b8aba1ab1d45806a0d1001fd62f
Pochang Lee <pochangl@usc.edu>
2019-08-24 16:33:25 +0800

    修正路徑最低值問題

--- DungeonGame/main.py
+++ DungeonGame/main.py
@@ -4,18 +4,18 @@ class Solution:
     def calculateMinimumHP(self, dungeon) -> int:
         goal = (len(dungeon) - 1, len(dungeon[0]) - 1)
 
-        def minimal_damage(x, y):
+        def minimal_damage(x, y, hp):
+            hp += dungeon[x][y]
             if (x, y) == goal:
-                return dungeon[x][y]
-            elif x == goal[x]:
-                value = minimal_damage(x, y + 1)
-            elif y == goal[y]:
-                value = minimal_damage(x + 1, y)
+                return hp
+            elif x == goal[0]:
+                value = minimal_damage(x, y + 1, hp)
+            elif y == goal[1]:
+                value = minimal_damage(x + 1, y, hp)
             else:
                 value = max(
-                    minimal_damage(x, y + 1),
-                    minimal_damage(x + 1, y)
+                    minimal_damage(x, y + 1, hp),
+                    minimal_damage(x + 1, y, hp)
                 )
-            return dungeon[x][y] + value
-
-        return max([1, 1 - minimal_damage(0, 0)])
+            return min(hp, value)
+        return max([1, 1 - minimal_damage(0, 0, 0)])
