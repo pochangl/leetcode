fe4f59e1183c5a32914dc935db255850c26c929d
Pochang Lee <pochangl@usc.edu>
2019-08-25 18:35:08 +0800

    在4格裡窮舉, 應該就夠了

--- SplitArrayWithSameAverage/test_main.py
+++ SplitArrayWithSameAverage/test_main.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from itertools import product
 from .brutal import BrutalSolution
 from .main import Solution
 
@@ -14,3 +15,7 @@ class TestSolution(TestCase):
 
     def test_case_basic_2(self):
         self.run_test([2, 3, 4, 5, 6, 7, 8])
+
+    def test_four(self):
+        for A in product(range(4), repeat=4):
+            self.run_test(A)
