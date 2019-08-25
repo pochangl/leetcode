343b36b075dd123bf6fca5c9598e4435eae9c7a3
Pochang Lee <pochangl@usc.edu>
2019-08-25 18:27:49 +0800

    用暴力法檢查

---
+++ SplitArrayWithSameAverage/brutal.py
@@ -0,0 +1,16 @@
+from itertools import combinations
+
+
+class BrutalSolution:
+    def splitArraySameAverage(self, A):
+        total = sum(A)
+
+        half = total / 2
+        length = len(A)
+
+        for size in range(length - 1):
+            for indexes in combinations(range(length), length - size):
+                subtotal = sum(A[index] for index in indexes)
+                if subtotal == half:
+                    return True
+        return False
