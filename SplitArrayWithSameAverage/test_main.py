2a5091fe9b7b3de899c65788884ffc9899b027fe
Pochang Lee <pochangl@usc.edu>
2019-08-25 18:30:57 +0800

    複製同一個版本, 然後拿去跑test case, 先做個修正的基礎

--- SplitArrayWithSameAverage/test_main.py
+++ SplitArrayWithSameAverage/test_main.py
@@ -1,14 +1,16 @@
 from unittest import TestCase
 from .brutal import BrutalSolution
+from .main import Solution
 
 
 class TestSolution(TestCase):
-    def run_test(self, A, expect):
-        answer = BrutalSolution().splitArraySameAverage(A)
+    def run_test(self, A):
+        expect = BrutalSolution().splitArraySameAverage(A)
+        answer = Solution().splitArraySameAverage(A)
         self.assertEqual(answer, expect)
 
     def test_case_basic_1(self):
-        self.run_test([1, 2, 3, 4, 5, 6, 7, 8], True)
+        self.run_test([1, 2, 3, 4, 5, 6, 7, 8])
 
     def test_case_basic_2(self):
-        self.run_test([2, 3, 4, 5, 6, 7, 8], False)
+        self.run_test([2, 3, 4, 5, 6, 7, 8])
