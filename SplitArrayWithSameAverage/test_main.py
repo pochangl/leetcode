80de295ea938840307f44ebcdac803ad5d24c13f
Pochang Lee <pochangl@usc.edu>
2019-08-25 19:14:18 +0800

    加入極端情境的測試

--- SplitArrayWithSameAverage/test_main.py
+++ SplitArrayWithSameAverage/test_main.py
@@ -6,9 +6,10 @@ from .main import Solution
 
 class TestSolution(TestCase):
     def run_test(self, A):
+        A = list(A)
         expect = BrutalSolution().splitArraySameAverage(A)
         answer = Solution().splitArraySameAverage(A)
-        self.assertEqual(answer, expect)
+        self.assertEqual(answer, expect, A)
 
     def test_case_basic_1(self):
         self.run_test([1, 2, 3, 4, 5, 6, 7, 8])
@@ -16,6 +17,25 @@ class TestSolution(TestCase):
     def test_case_basic_2(self):
         self.run_test([2, 3, 4, 5, 6, 7, 8])
 
+    def test_length_1(self):
+        '長度一的話都不ok'
+        solver = Solution().splitArraySameAverage
+
+        for value in range(1, 100):
+            self.assertEqual(solver([value]), False)
+
+    def test_zeroes(self):
+        '零不管多長都ok'
+        solver = Solution().splitArraySameAverage
+        for value in range(1, 100):
+            self.assertEqual(solver([0] * value), True)
+
     def test_four(self):
+        '雙數長度窮舉'
         for A in product(range(4), repeat=4):
+            self.run_test(A)
+
+    def test_five(self):
+        '單數長度 array 窮舉'
+        for A in product(range(5), repeat=5):
             self.run_test(A)
